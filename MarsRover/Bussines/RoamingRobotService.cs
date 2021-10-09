dd4fb229e3ff636118c1b3554d84584e940899ca
yasin çınar <yasin001905@gmail.com>
2021-10-10 00:22:51 +0300

    Roaming robot service created

---
+++ MarsRover/Bussines/RoamingRobotService.cs
@@ -0,0 +1,19 @@
+﻿using MarsRover.Bussines.Services;
+using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Bussines
+{
+    public class RoamingRobotService : IRoamingRobotService
+    {
+        public string ScoutRobot(Point Location, RoamingRobot roamingRobot, string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarsRover/Bussines/Services/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using MarsRover.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Bussines.Services
+{
+    public interface IRoamingRobotService
+    {
+        public string ScoutRobot(Point Location, RoamingRobot roamingRobot, string command); // keşif robotu
+
+    }
+}
