f75a0d96132168bdc01c054865be5252d5fd57b8
yasin çınar <yasin001905@gmail.com>
2021-10-10 00:04:51 +0300

    Roaming robot model created

---
+++ MarsRover/Models/RoamingRobot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Models
+{
+    public class RoamingRobot  //Gezici Robot
+    {
+        //Robotun X pozisyonu
+        public int PositionXPlane { get; set; }
+        //Robotun Y pozisyonu
+        public int PositionYPlane { get; set; }
+        //Robotun baktığı yön
+        public char Direction { get; set; }
+    }
+}
