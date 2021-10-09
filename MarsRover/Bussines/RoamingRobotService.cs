b70ad73f7ec1f0ebf76b09bd44e67a580dd3d98d
yasin çınar <yasin001905@gmail.com>
2021-10-10 00:38:19 +0300

    interface name to edit and Service Command Control  created

--- MarsRover/Bussines/RoamingRobotService.cs
+++ MarsRover/Bussines/RoamingRobotService.cs
@@ -13,7 +13,136 @@ namespace MarsRover.Bussines
     {
         public string ScoutRobot(Point Location, RoamingRobot roamingRobot, string command)
         {
-            throw new NotImplementedException();
+            if (!CheckIfRobotLocation(Location, roamingRobot))
+            {
+                return "Robot konumu Lokasyonun Dışında Tanımlanmıştır";
+            }
+            CommandControl(roamingRobot, command);
+            return (roamingRobot.PositionXPlane + " " + roamingRobot.PositionYPlane + " " + roamingRobot.Direction);
         }
+
+        private void CommandControl(RoamingRobot roamingRobot, string command)
+        {
+            for (int i = 0; i < command.Length; i++)
+            {
+                if (CheckCommand(command[i], roamingRobot))
+                {
+                    if (CheckIfRoamingRobotXDirection(roamingRobot.Direction))// Robotun X pozisyonundayken baktığı yöne göre X değerini değiştirme
+                    {
+                        if (roamingRobot.Direction == 'E')
+                        {
+                            roamingRobot.PositionXPlane++;
+                        }
+                        else
+                        {
+                            roamingRobot.PositionXPlane--;
+                        }
+                    }
+                    else if (CheckIfRoamingRobotYDirection(roamingRobot.Direction))// Robotun Y pozisyonundayken baktığı yöne göre Y değerini değiştirme
+                    {
+                        if (roamingRobot.Direction == 'N')
+                        {
+                            roamingRobot.PositionYPlane++;
+                        }
+                        else
+                        {
+                            roamingRobot.PositionYPlane--;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+
+        #region Komut Kontrolü ile Robotun yönünü belirleme
+        private bool CheckCommand(char command, RoamingRobot roamingRobot)
+        {
+
+            if (command == 'M')
+            {
+                return true;
+            }
+            else if (command == 'R')
+            {
+                if (roamingRobot.Direction == 'W')
+                {
+                    roamingRobot.Direction = 'N';
+                }
+                else if (roamingRobot.Direction == 'N')
+                {
+                    roamingRobot.Direction = 'E';
+                }
+                else if (roamingRobot.Direction == 'E')
+                {
+                    roamingRobot.Direction = 'S';
+                }
+                else
+                {
+                    roamingRobot.Direction = 'W';
+                }
+            }
+            else if (command == 'L')
+            {
+                if (roamingRobot.Direction == 'N')
+                {
+                    roamingRobot.Direction = 'W';
+                }
+                else if (roamingRobot.Direction == 'W')
+                {
+                    roamingRobot.Direction = 'S';
+                }
+                else if (roamingRobot.Direction == 'S')
+                {
+                    roamingRobot.Direction = 'E';
+                }
+                else
+                {
+                    roamingRobot.Direction = 'N';
+                }
+            }
+
+
+            return false;
+        }
+        #endregion
+
+
+        #region Robotun Lokasyon içerisinde bulunup bulunmama durumu
+        private bool CheckIfRobotLocation(Point location, RoamingRobot roamingRobot)
+        {
+            if (roamingRobot.PositionXPlane > location.X)
+            {
+                return false;
+            }
+            if (roamingRobot.PositionYPlane > location.Y)
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+        #region Gezicinin Y Yönünde Bulunup Bulunmama Durumu
+        private bool CheckIfRoamingRobotYDirection(char direction)
+        {
+            if (direction == 'N' || direction == 'S')
+            {
+                return true;
+            }
+            return false;
+
+        }
+        #endregion
+        #region Gezicinin X Yönünde Bulunup Bulunmama Durumu
+        private bool CheckIfRoamingRobotXDirection(char direction)
+        {
+            if (direction == 'E' || direction == 'W')
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
     }
 }
